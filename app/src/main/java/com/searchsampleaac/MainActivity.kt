0b0918ce431243cca6fd27ce6177728def710617
AramJu <aaadkfka@naver.com>
2023-02-21 18:13:04 +0900

    databinding setting

--- app/src/main/java/com/searchsampleaac/MainActivity.kt
+++ app/src/main/java/com/searchsampleaac/MainActivity.kt
@@ -2,12 +2,15 @@ package com.searchsampleaac
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.searchsampleaac.databinding.ActivityMainBinding
 import dagger.hilt.android.AndroidEntryPoint
 
 @AndroidEntryPoint
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        val binding = ActivityMainBinding.inflate(layoutInflater)
+        val view = binding.root
+        setContentView(view)
     }
 }
