0e7068f9f947c0cee6edbcc244364e47146a301a
AramJu <aaadkfka@naver.com>
2023-02-21 19:27:04 +0900

    clean up code

--- app/src/main/java/com/searchsampleaac/data/SearchRepository.kt
+++ app/src/main/java/com/searchsampleaac/data/SearchRepository.kt
@@ -4,6 +4,7 @@ import androidx.paging.Pager
 import androidx.paging.PagingConfig
 import androidx.paging.PagingData
 import com.searchsampleaac.api.SearchService
+import com.searchsampleaac.data.model.RepoItemModel
 import com.searchsampleaac.data.model.SearchItemModel
 import kotlinx.coroutines.flow.Flow
 import javax.inject.Inject
@@ -14,6 +15,13 @@ class SearchRepository @Inject constructor(private val service: SearchService) {
         return Pager(
             config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
             pagingSourceFactory = { SearchPagingSource(service, query) }
+        ).flow
+    }
+
+    fun getUserRepoStream(username: String): Flow<PagingData<RepoItemModel>> {
+        return Pager(
+            config = PagingConfig(enablePlaceholders = false, pageSize = NETWORK_PAGE_SIZE),
+            pagingSourceFactory = { RepoPagingSource(service, username) }
         ).flow
     }
 
